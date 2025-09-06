ef9c9d46ad0745d9324221a0e5ae059dfed5655b
Amitav Roy Chowdhury <amitavrchy01@gmail.com>
2025-09-06 10:18:10 +0600

    Made a reducer

---
+++ src/redux/features/task/taskSlice.ts
@@ -0,0 +1,26 @@
+import type ITask from "@/types";
+import { createSlice } from "@reduxjs/toolkit";
+
+interface InitialState {
+    task: ITask[]
+}
+
+const initialState : InitialState = {
+    task: [
+        {
+            id: "1",
+            title: "Initialize Something",
+            description: "Create homepage and routing",
+            dueDate: "2025-11",
+            isCompleted: false,
+            priority: "High"
+        },
+    ]
+}
+const taskSlice = createSlice({
+    name: "task",
+    initialState,
+    reducers: {}
+})
+export default taskSlice.reducer
+
