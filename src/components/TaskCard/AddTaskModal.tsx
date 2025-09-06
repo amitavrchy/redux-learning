9d66364f626df9be8f9173b065606723bd787687
Amitav Roy Chowdhury <amitavrchy01@gmail.com>
2025-09-06 16:55:09 +0600

    form done

--- src/components/TaskCard/AddTaskModal.tsx
+++ src/components/TaskCard/AddTaskModal.tsx
@@ -11,6 +11,13 @@ import {
 import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form"
 import { useForm } from "react-hook-form"
 import { Input } from "../ui/input"
+import { Textarea } from "../ui/textarea"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
+import { cn } from "@/lib/utils"
+import { CalendarIcon } from "lucide-react"
+import { Calendar } from "../ui/calendar"
+import { format } from "date-fns"
 
 export function AddTaskModal() {
     const form = useForm()
@@ -32,12 +39,82 @@ export function AddTaskModal() {
                             <FormField
                                 control={form.control}
                                 name="title"
-                                render={({field}) => (
+                                render={({ field }) => (
                                     <FormItem>
-                                        <FormLabel />
+                                        <FormLabel>Title</FormLabel>
                                         <FormControl className="mb-5">
-                                            <Input {...field}/>
+                                            <Input {...field} value={field.value || ""} />
                                         </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="description"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Description</FormLabel>
+                                        <FormControl className="mb-5">
+                                            <Textarea {...field} value={field.value || ""} />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="priority"
+                                render={({ field }) => (
+                                    <FormItem className="w-full">
+                                        <FormLabel>Priority</FormLabel>
+                                        <Select onValueChange={field.onChange}>
+                                            <FormControl className="w-full">
+                                                <SelectTrigger>
+                                                    <SelectValue placeholder="Select a Priority to display" />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent className="w-full">
+                                                <SelectItem value="low">Low</SelectItem>
+                                                <SelectItem value="medium">Medium</SelectItem>
+                                                <SelectItem value="high">High</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="dob"
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-col my-5">
+                                        <FormLabel>Due Date</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <FormControl>
+                                                    <Button
+                                                        variant={"outline"}
+                                                        className={cn(
+                                                            "pl-3 text-left font-normal",
+                                                            !field.value && "text-muted-foreground"
+                                                        )}
+                                                    >
+                                                        {field.value ? (
+                                                            format(field.value, "PPP")
+                                                        ) : (
+                                                            <span>Pick a date</span>
+                                                        )}
+                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                    </Button>
+                                                </FormControl>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-auto p-0" align="start">
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    captionLayout="dropdown"
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
                                     </FormItem>
                                 )}
                             />
